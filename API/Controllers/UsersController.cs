cd68b67802a25f221e49cbdccef92a8a157f0178
Danyil Terentiev <dt210204@gmail.com>
2023-02-24 19:49:19 +0200

    Initial commit with db and GET requests

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DatingAppContext _context;
+        public UsersController(DatingAppContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<User> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ API/Data/DatingAppContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DatingAppContext : DbContext
+    {
+        public DatingAppContext(DbContextOptions<DatingAppContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
