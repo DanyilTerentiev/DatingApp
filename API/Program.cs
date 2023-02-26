428d00d8afb4173c996bc8aca89c2222e09ffbbc
Danyil Terentiev <dt210204@gmail.com>
2023-02-26 13:59:29 +0200

    Created angular part

--- API/Program.cs
+++ API/Program.cs
@@ -4,14 +4,14 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DatingAppContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 var app = builder.Build();
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200") );
 app.MapControllers();
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.Run();
